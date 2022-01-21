1110a6e9dc7f64404b18710ecfce3458dd5fbc64
presqueasmr <auriane1103@gmail.com>
2022-01-21 18:49:18 +0100

    groupe et artistes

--- JeuEssai.sql
+++ JeuEssai.sql
@@ -11,7 +11,7 @@
 --	BDD MySQL
 */
 --
--- Base de données: `GESTIONNOTES`
+-- Base de données: mediatheq22
 --
 USE mediatheq22;
 
@@ -39,46 +39,12 @@ INSERT INTO CLASSE (numClas, libClas) VALUES ("CLAS0006", "LP MIAMI Tarbes");
 -- (<numEtu, char(10),>, <numClas, char(6),>, <nomEtu, char(30),>, <prenomEtu, char(30),>, <dtNaisEtu, datetime,>
  ,<libRueEtu, char(32),>, <cdPostEtu, char(5),>, <libVilEtu, char(26),>, <tailleEtu, numeric(3,0),>)
 */
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(001, 'CLAS0005', 'BOIS', 'Mimi', '2005-03-11 00:00:00', "Bat D Rue des copains d'abord", "02000", "SOISSONS",169);
--- ('3DH4UN', 'CLAS0005', 'BOIS', 'Mimi', '2005-03-11 00:00:00', "Bat D Rue des copains d'abord", "02000", "SOISSONS",169);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(002, 'CLAS0003', 'MAUROIZ', 'Pierre', '2004-01-01 00:00:00', "Avenue de la progression", "33000", "BORDEAUX", 178);
--- ('A01267', 'CLAS0003', 'MAUROIZ', 'Pierre', '2004-01-01 00:00:00', "Avenue de la progression", "33000", "BORDEAUX", 178);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(003, 'CLAS0004', 'MICHOAL', 'Albert', '2005-02-22 00:00:00', "Rue des tulipes rouges", "33000", "BORDEAUX", 167);
--- ('D3XM09', 'CLAS0004', 'MICHOAL', 'Albert', '2005-02-22 00:00:00', "Rue des tulipes rouges", "33000", "BORDEAUX", 167);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(004, NULL, 'COLLINS', 'Phil', '2005-09-04 00:00:00', "Place de la musique", "75003", "PARIS", 183);
--- ('GHPLQ1', NULL, 'COLLINS', 'Phil', '2005-09-04 00:00:00', "Place de la musique", "75003", "PARIS", 183);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(005, 'CLAS0003', 'DUFOUR', 'Michel', '2005-08-08 00:00:00', "Rue des Roses", "33700", "MÉRIGNAC", NULL);
--- ('H23KK1', 'CLAS0003', 'DUFOUR', 'Michel', '2005-08-08 00:00:00', "Rue des Roses", "33700", "MÉRIGNAC", NULL);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(006, 'CLAS0004', 'CHAUROND', 'Wiefried', '2004-02-20 00:00:00', "23, avenue des aiguilles", "33320", "BRUGES", 176);
--- ('KV7N03', 'CLAS0004', 'CHAUROND', 'Wiefried', '2004-02-20 00:00:00', "23, avenue des aiguilles", "33320", "BRUGES", 176);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(007, 'CLAS0004', 'MOUCHOUD', 'Xavier', '2005-04-14 00:00:00', "Rue des roses jaunes", "33000", "BORDEAUX", NULL);
--- ('LK33D2', 'CLAS0004', 'MOUCHOUD', 'Xavier', '2005-04-14 00:00:00', "Rue des roses jaunes", "33000", "BORDEAUX", NULL);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(008, 'CLAS0003', 'QUINAZ', 'Peter', '2004-09-09 00:00:00', "Rue des poissons", "33110", "LE BOUSCAT", 169);
--- ('QR5P09', 'CLAS0003', 'QUINAZ', 'Peter', '2004-09-09 00:00:00', "Rue des poissons", "33110", "LE BOUSCAT", 169);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(009, 'CLAS0005', 'KOUAZ', 'Alan', '2004-02-03 00:00:00', "Impasse Filibert", "75011", "PARIS", 174);
--- ('SQ980A', 'CLAS0005', 'KOUAZ', 'Alan', '2004-02-03 00:00:00', "Impasse Filibert", "75011", "PARIS", 174);
-INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, cdPostEtu, libVilEtu, tailleEtu)
-		VALUES
-(010, 'CLAS0004', 'ROUX', 'Raymond', '2003-07-23 00:00:00', "Rue des roses roses", "33110","LE BOUSCAT", 172);
--- ('WM56J0', 'CLAS0004', 'ROUX', 'Raymond', '2003-07-23 00:00:00', "Rue des roses roses", "33110","LE BOUSCAT", 172);
+INSERT INTO ARTISTE (idArt, idGp, nomArt, prenomArt) VALUES (001, 100, 'AZNAVOUR', 'Choucou');
+INSERT INTO ARTISTE (idArt, idGp, nomArt, prenomArt) VALUES (002, 200, 'SOFIANE', 'Bibi');
+INSERT INTO ARTISTE (idArt, idGp, nomArt, prenomArt) VALUES (003, 300, 'GIMS', 'Maître');
+INSERT INTO ARTISTE (idArt, idGp, nomArt, prenomArt) VALUES (004, 400, 'BENT', 'Amel');
+INSERT INTO ARTISTE (idArt, idGp, nomArt, prenomArt) VALUES (005, 500, 'MAPIOUX', 'Sosomanes');
+INSERT INTO ARTISTE (idArt, idGp, nomArt, prenomArt) VALUES (006, 600, 'MARIA', 'Choucou');
 --
 -- Etudiant aussi enseignant
 --
@@ -104,33 +70,13 @@ INSERT INTO ETUDIANT (numEtu, numClas, nomEtu, prenomEtu, dtNaisEtu, libRueEtu, 
 --
 -- (<dtCtrl, datetime,>)
 */
-INSERT INTO DATE (dtCtrl) VALUES ('2012-11-09 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-01 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-12 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-12 09:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-12 11:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-13 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-17 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-18 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-01-11 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-01-13 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-01-17 00:00:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-02-22 14:30:00');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-12-12 00:00:00');
---
-INSERT INTO DATE (dtCtrl) VALUES ('2012-11-09 00:45:40');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-01 05:00:40');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-12 22:20:59');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-12 09:00:12');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-12 11:00:17');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-13 05:05:05');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-17 07:00:33');
-INSERT INTO DATE (dtCtrl) VALUES ('2012-12-18 03:00:04');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-01-11 08:10:18');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-01-13 14:06:19');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-01-17 13:00:35');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-02-22 14:30:56');
-INSERT INTO DATE (dtCtrl) VALUES ('2013-12-12 20:04:07');
+INSERT INTO groupe (idGp, nomGp, dtCreaGp) VALUES (100);
+INSERT INTO groupe (idGp, nomGp, dtCreaGp) VALUES (200);
+INSERT INTO groupe (idGp, nomGp, dtCreaGp) VALUES (300);
+INSERT INTO groupe (idGp, nomGp, dtCreaGp) VALUES (400);
+INSERT INTO groupe (idGp, nomGp, dtCreaGp) VALUES (500);
+INSERT INTO groupe (idGp, nomGp, dtCreaGp) VALUES (600);
+
 
 /*
 --
