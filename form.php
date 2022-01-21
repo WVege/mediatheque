6cbfe7efd6173cd640cb47cf71d8bdcbb2935ef6
Quentin Wegera <quentin.wegera@mmibordeaux.com>
2022-01-21 12:39:47 +0100

    formulaire cookies

--- form.php
+++ form.php
@@ -1,9 +1,25 @@
 <?php
+
+setcookie('connection', $_POST['email'], time() + 365*24*3600, null, null, false, true); // On créé le cookie, c'est IMPORTANT !
+// echo(urldecode($_COOKIE["connection"]));²
+setcookie('pseudo', $_POST['firstname'], time() + 365*24*3600, null, null, false, true);
+
+setcookie('name', $_POST['name'], time() + 365*24*3600, null, null, false, true);
 ?>
 
 <body>
       <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-            <input type="text" name="name"> <!--Ici le nom de notre zone de texte est name-->
-            <input type="submit" value="Ajouter">
+            
+            <label for="name">Entrez votre email: </label>
+            <input type="text" name="email" value="<?php if(isset($_COOKIE['email']));?>"> </br>  <!--Ici le nom de notre zone de texte est name-->
+
+            <label for="name">Entrez votre prénom: </label>
+             <input type="text" name="firstname" value="<?php if(isset($_COOKIE['firstname']));?>"> </br>  <!--Ici le nom de notre zone de texte est name-->
+
+            <label for="name">Entrez votre nom: </label>
+            <input type="text" name="name" value="<?php if(isset($_COOKIE['name']));?>"> </br>  <!--Ici le nom de notre zone de texte est name-->
+
+            <input type="submit" value="Valider">
       </form>
 </body>
+
